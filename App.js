d241b53e9e16b302bacb9b43195a9f4910314e9d
robertjanzen <robertjanzenbc@gmail.com>
2018-11-03 10:50:24 -0700

    Add Root Stack to App.js
    Ran npm install --save react-navigation

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+import HomeScreen from "./components/HomeScreen"
+
+const RootStack = createStackNavigator {
+    {
+        Home: {
+            screen: HomeScreen
+        }
+    },
+    {
+        initialRouteName: 'Home',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#000000'
+            },
+        }
+    },
+}
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return <RootStack />;
   }
 }
 
