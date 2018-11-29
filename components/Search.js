394e830aa9e5587aa93f8dc68c4cac5662a67212
jsalisi <jsalisi@gmail.com>
2018-11-28 23:28:06 -0800

    Added new homepage style + placeholder

--- components/Search.js
+++ components/Search.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {Dimensions, StyleSheet, Text, View} from 'react-native';
-import {SearchBar, ButtonGroup} from 'react-native-elements'
+import {Dimensions, StyleSheet, Text, TouchableWithoutFeedback, Image, ScrollView, FlatList, View} from 'react-native';
+import {SearchBar, Header} from 'react-native-elements'
 import {LinearGradient} from 'expo';
 import * as Animatable from 'react-native-animatable';
 import {GOOGLE_BOOKS_API_KEY, TASTE_API_KEY, THE_MOVIE_DB_API_KEY} from 'react-native-dotenv'
 // import DiscoverComponent from './DiscoverComponent';
 
-export const bookReq = `https://tastedive.com/api/similar?k=${TASTE_API_KEY}&type=books&info=true&limit=7&q=book:`;
-export const movieReq = `https://tastedive.com/api/similar?k=${TASTE_API_KEY}&type=movies&info=true&limit=7&q=movie:`;
-export const showReq = `https://tastedive.com/api/similar?k=${TASTE_API_KEY}&type=shows&info=true&limit=7&q=`;
+export const bookReq = `https://tastedive.com/api/similar?k=${TASTE_API_KEY}&type=books&info=true&limit=10&q=book:`;
+export const movieReq = `https://tastedive.com/api/similar?k=${TASTE_API_KEY}&type=movies&info=true&limit=10&q=movie:`;
+export const showReq = `https://tastedive.com/api/similar?k=${TASTE_API_KEY}&type=shows&info=true&limit=10&q=`;
 
-const bookRequest = `https://www.googleapis.com/books/v1/volumes?key=${GOOGLE_BOOKS_API_KEY}&maxResults=3&q=`;
+const bookRequest = `https://www.googleapis.com/books/v1/volumes?key=${GOOGLE_BOOKS_API_KEY}&maxResults=10&q=`;
 const movieRequest = `https://api.themoviedb.org/3/search/movie?api_key=${THE_MOVIE_DB_API_KEY}&query=`;
 const showRequest = `https://api.themoviedb.org/3/search/tv?api_key=${THE_MOVIE_DB_API_KEY}&query=`;
 
@@ -35,10 +35,10 @@ export default class Search extends React.Component {
             bookList: [],
             movieGenres: [],
             tvGenres: [],
+            searchOpen: false
         }
         this.searchText = this.searchText.bind(this);
     }
-    
 
     componentWillMount() {
         Promise.all([
@@ -50,6 +50,10 @@ export default class Search extends React.Component {
                 tvGenres: res[1]
             });
         });
+    }
+
+    componentDidMount() {
+        this.menu.transitionTo({ top: screenHeight * 0.15 })
     }
 
     searchText(text) {
@@ -164,31 +168,82 @@ export default class Search extends React.Component {
     }
 
     handleViewRef = ref => this.view = ref;
+    handleListRef = ref => this.menu = ref;
+
+    renderLeftComponent = () => {
+        return(
+            <Animatable.Text animation="pulse" iterationCount={"infinite"} direction="alternate" style={styles.text}>
+                Entertain Me
+                <Text style={styles.text_emphasis}>
+                    !
+                    </Text>
+            </Animatable.Text>  
+        );
+    }
+
+    renderRightComponent = () => {
+        return(
+            <TouchableWithoutFeedback onPress={() => {
+                if (this.state.searchOpen === true) {
+                    this.view.transitionTo({ top: screenHeight * 0.05 })
+                    this.menu.transitionTo({ top: screenHeight * 0.15 })
+                    this.setState({
+                        searchOpen: false
+                    });
+                } else {
+                    this.view.transitionTo({ top: screenHeight * 0.15 })
+                    this.menu.transitionTo({ top: screenHeight * 0.25 })
+                    this.setState({
+                        searchOpen: true
+                    });
+                }
+            }}>
+                <Image style={styles.button} source={require('../assets/search.png')}/>
+            </TouchableWithoutFeedback>
+        );
+    }
 
     render() {
         const {navigate} = this.props.navigation;
 
         return (
+            <LinearGradient colors={['#000000', '#323232']}
+                style={{
+                    position: 'absolute',
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    height: (Dimensions.get('window').height),
+                    width: (Dimensions.get('window').width),
+                    }}
+            >
             <View style={styles.container}>
                 {/* <StatusBar hidden /> */}
-                <LinearGradient
-                    colors={['#000000', '#323232']}
-                    style={{
-                        position: 'absolute',
-                        left: 0,
-                        right: 0,
-                        top: 0,
-                        height: (Dimensions.get('window').height),
-                        width: (Dimensions.get('window').width),
-                    }}
-                />
-                <Animatable.View style={styles.items} ref={this.handleViewRef}>
-                    <Animatable.Text animation="pulse" iterationCount={"infinite"} direction="alternate" style={styles.text}>
-                        Entertain Me
-                        <Text style={styles.text_emphasis}>
-                            !
-                        </Text>
-                    </Animatable.Text>
+                
+                <Animatable.View style={StyleSheet.absoluteFill} style={{position: 'relative'}} ref={this.handleListRef}>
+                <ScrollView>
+                    <Text style={styles.cat_text}> Popular </Text>
+                    <FlatList
+                        horizontal={true}
+                        contentContainerStyle={styles.row}
+                        data={[{key: 'PLACEHOLDER 1'}, {key: 'PLACEHOLDER 2'}, {key: 'PLACEHOLDER 3'}]}
+                        renderItem={({item}) => <View style={styles.box}><Text>{item.key}</Text></View>}/>  
+                    <Text style={styles.cat_text}> New </Text>
+                    <FlatList
+                        horizontal={true}
+                        contentContainerStyle={styles.row}
+                        data={[{key: 'PLACEHOLDER 1'}, {key: 'PLACEHOLDER 2'}, {key: 'PLACEHOLDER 3'}]}
+                        renderItem={({item}) => <View style={styles.box}><Text>{item.key}</Text></View>}/>  
+                    <Text style={styles.cat_text}> Discover </Text>
+                    <FlatList
+                        horizontal={true}
+                        contentContainerStyle={styles.row}
+                        data={[{key: 'PLACEHOLDER 1'}, {key: 'PLACEHOLDER 2'}, {key: 'PLACEHOLDER 3'}]}
+                        renderItem={({item}) => <View style={styles.box}><Text>{item.key}</Text></View>}/>     
+                </ScrollView>
+                </Animatable.View>
+
+                <Animatable.View style={styles.searchBar} ref={this.handleViewRef}>
                     <SearchBar
                         containerStyle={styles.search}
                         round
@@ -199,32 +254,18 @@ export default class Search extends React.Component {
                         icon={{ type: 'font-awesome', name: 'search', color: 'red' }}
                         onChangeText={(text) => this.searchText(text)}
                         placeholder='What are you interested in?'
-                        onFocus={() => this.view.transitionTo({ top: screenHeight * 0.10 })}
-                        onEndEditing={() => this.view.transitionTo({ top: screenHeight * 0.32 })}
-                    />
-                    <ButtonGroup
-                        textStyle={{color: 'white'}}
-                        buttons={['Search by title', 'Search by keyword']}
-                        containerStyle={styles.menu}
-                        onPress={() => {
+                        onSubmitEditing={() => {
                                 this.navToSearchResults()
                             }}
                     />
-                    <ButtonGroup
-                        textStyle={{color: 'white'}}
-                        buttons={['Discover']}
-                        containerStyle={styles.button}
-                        onPress={() => {
-                                this.navToDiscover()
-                            }}
-                    />
                 </Animatable.View>
-                {/*{this.state.movieGenres.length > 0*/}
-                    {/*? <FiltersComponent movieGenres={this.state.movieGenres} tvGenres={this.state.tvGenres}/>*/}
-                    {/*? <DiscoverComponent movieGenres={this.state.movieGenres} tvGenres={this.state.tvGenres}/>*/}
-                    {/*:*/}
-                    {/*<View/>}*/}
+                <Header
+                    outerContainerStyles={styles.header_item}
+                    leftComponent={this.renderLeftComponent()}
+                    rightComponent={this.renderRightComponent()}
+                />          
             </View>
+            </LinearGradient>
         );
     }
 }
@@ -232,50 +273,69 @@ export default class Search extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 2,
-        backgroundColor: '#323232',
+        backgroundColor: 'transparent',
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
     },
-    items: {
+    header_item: {
+        backgroundColor:  'black',
+        position: 'absolute',
+        top: 0,
+        height: screenHeight * 0.15,
+        width: screenWidth,
+        borderBottomColor: 'transparent'
+    },
+    searchBar: {
         flex: 1,
         position: 'absolute',
-        top: screenHeight * 0.32,
+        top: screenHeight * 0.05,
         width: screenWidth
     },
     text_emphasis: {
-        fontSize: 50,
+        fontSize: 30,
         fontWeight: 'bold',
         color: 'red',
     },
     text: {
-        fontSize: 50,
+        fontSize: 30,
         fontWeight: 'bold',
         color: 'white',
-        textAlign: 'center',
-        marginBottom: 10
+        textAlign: 'center'
     },
     search: {
         backgroundColor: 'transparent',
-        left: screenWidth * 0.10,
-        width: screenWidth * 0.80,
+        left: screenWidth * 0.025,
+        width: screenWidth * 0.95,
         borderColor: '#323232',
         borderBottomColor: 'transparent',
         borderTopColor: 'transparent',
         marginBottom: 10
     },
-    menu: { 
-        left: screenWidth * 0.10,
-        width: screenWidth * 0.76,
+    button: {
+        width: 45,
         alignItems:  'center',
         justifyContent: 'center',
         backgroundColor: 'black'
     },
-    button: {
-        left: screenWidth * 0.10,
-        width: screenWidth * 0.76,
-        alignItems:  'center',
-        justifyContent: 'center',
-        backgroundColor: 'black'
-    }
+    cat_text: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'left',
+    },
+    row: {
+        height: screenHeight * 0.30,
+        justifyContent: 'flex-start',
+    },
+    box: {
+        width: screenWidth * 0.35,
+        height: '100%',
+        margin: screenWidth * 0.01,
+        marginTop: 0,
+        backgroundColor: 'gray',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        borderColor: 'white'
+    },
 });
