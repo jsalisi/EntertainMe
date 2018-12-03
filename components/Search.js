5e7a61db32e694b1ec1eeda5d3d96ec4816c6dd0
emilieez <emilie.m.zhang@gmail.com>
2018-12-03 10:15:10 -0800

    Refactor

--- components/Search.js
+++ components/Search.js
@@ -222,19 +222,21 @@ export default class Search extends React.Component {
         if (idx === 0) {
             return (
                 <ScrollView>
-                    <FlatlistComponent type={'Most Popular Movies'} movieGenres={this.state.movieGenres} navigation={this.props.navigation}/>
-                    <FlatlistComponent type={'Most Popular Shows'} tvGenres={this.state.tvGenres} navigation={this.props.navigation}/>
+                    <FlatlistComponent type={'Most Popular Movies'} movieGenres={this.state.movieGenres}
+                                       navigation={this.props.navigation}/>
+                    <FlatlistComponent type={'Most Popular Shows'} tvGenres={this.state.tvGenres}
+                                       navigation={this.props.navigation}/>
                 </ScrollView>
             )
         } else if (idx === 1) {
-            return (
-                <View style={{height: screenHeight, width: screenWidth}}>
-                    {this.state.movieGenres.length > 0 ?
-                        <DiscoverComponent movieGenres={this.state.movieGenres} tvGenres={this.state.tvGenres} navigation={this.props.navigation}/>
-                        :
-                        <View/>}
-                </View>
-            )
+            if (this.state.movieGenres.length > 0) {
+                return (
+                    <View style={{height: screenHeight, width: screenWidth}}>
+                        <DiscoverComponent movieGenres={this.state.movieGenres} tvGenres={this.state.tvGenres}
+                                           navigation={this.props.navigation}/>
+                    </View>
+                )
+            }
         }
     }
 
@@ -296,75 +298,75 @@ export default class Search extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 2,
-    backgroundColor: "transparent",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  header_item: {
-    backgroundColor: "black",
-    position: "absolute",
-    top: 0,
-    height: screenHeight * 0.15,
-    width: screenWidth,
-    borderBottomColor: "transparent"
-  },
-  searchBar: {
-    flex: 1,
-    position: "absolute",
-    top: screenHeight * 0.05,
-    width: screenWidth
-  },
-  text_emphasis: {
-    fontSize: 30,
-    fontWeight: "bold",
-    color: "red"
-  },
-  text: {
-    fontSize: 30,
-    fontWeight: "bold",
-    color: "white",
-    textAlign: "center"
-  },
-  search: {
-    backgroundColor: "transparent",
-    left: screenWidth * 0.025,
-    width: screenWidth * 0.95,
-    borderColor: "#323232",
-    borderBottomColor: "transparent",
-    borderTopColor: "transparent",
-    marginBottom: 10
-  },
-  menu: {
-    marginLeft: screenWidth * 0.025,
-    width: screenWidth * 0.95,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "black",
-    marginBottom: screenHeight * 0.025
-  },
-  button: {
-    width: 45,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "black"
-  },
-  cat_text: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "white",
-    textAlign: "left"
-  },
-  box: {
-    width: screenWidth * 0.9,
-    height: screenHeight * 0.25,
-    margin: screenWidth * 0.01,
-    marginTop: 0,
-    backgroundColor: "gray",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    borderColor: "white"
-  }
+    container: {
+        flex: 2,
+        backgroundColor: "transparent",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    header_item: {
+        backgroundColor: "black",
+        position: "absolute",
+        top: 0,
+        height: screenHeight * 0.15,
+        width: screenWidth,
+        borderBottomColor: "transparent"
+    },
+    searchBar: {
+        flex: 1,
+        position: "absolute",
+        top: screenHeight * 0.05,
+        width: screenWidth
+    },
+    text_emphasis: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "red"
+    },
+    text: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "white",
+        textAlign: "center"
+    },
+    search: {
+        backgroundColor: "transparent",
+        left: screenWidth * 0.025,
+        width: screenWidth * 0.95,
+        borderColor: "#323232",
+        borderBottomColor: "transparent",
+        borderTopColor: "transparent",
+        marginBottom: 10
+    },
+    menu: {
+        marginLeft: screenWidth * 0.025,
+        width: screenWidth * 0.95,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "black",
+        marginBottom: screenHeight * 0.025
+    },
+    button: {
+        width: 45,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "black"
+    },
+    cat_text: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "white",
+        textAlign: "left"
+    },
+    box: {
+        width: screenWidth * 0.9,
+        height: screenHeight * 0.25,
+        margin: screenWidth * 0.01,
+        marginTop: 0,
+        backgroundColor: "gray",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        borderColor: "white"
+    }
 });
